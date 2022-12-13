fdf37c8a92a96ddea34dc7152e87806da1185711
Leping <lli543@wisc.edu>
2022-12-13 11:29:52 -0600

    add followup

--- src/Components/Post.js
+++ src/Components/Post.js
@@ -3,11 +3,24 @@ import React from 'react'
 import DOMPurify from 'dompurify'
 import ReactHtmlParser from 'react-html-parser'
 import Avatar from 'react-avatar'
-
+import LikeButton from './Like'
+import FollowUp from './Follow'
 import styles from './post.module.css'
 
 const Post = ({ profileImage, name, content, date, time }) => {
   const clean = DOMPurify.sanitize(content)
+  const followups = [{
+    name: "Unkown",
+    content: "<p>I doubt!</p>",
+    },
+    {
+      name: "Unkown",
+      content: "<p>I agree!</p>",
+    }
+  ];
+  const followUps= followups.map((follow) => (
+    <FollowUp name={follow.name} content={follow.content}/>
+  ));
 
   return (
     <div className='panel panel-default p-3'>
@@ -30,9 +43,11 @@ const Post = ({ profileImage, name, content, date, time }) => {
             <p className={`mb-0 text-right ${styles.dateTime}`}>{time}</p>
           </div>
           <div className='col-2' style={{marginTop: 10}}>
-            <p> Like </p>
-            <p> Reply </p>
+            <LikeButton/>
           </div>
+        </div>
+        <div className='col'>
+          {followUps}
         </div>
       </div>
     </div>
